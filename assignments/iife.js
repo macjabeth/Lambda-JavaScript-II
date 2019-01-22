3015ede1cb8a7c0d302c5678b67df516082c4d83
macjabeth <macbethjonathan@gmail.com>
2019-01-22 22:23:51 +0000

    complete stretch: IIFEs

---
+++ assignments/iife.js
@@ -0,0 +1,45 @@
+// classical approach
+
+(() => {
+  console.log('Variation 1');
+})();
+
+(() => {
+  console.log('Variation 2');
+})();
+
+// expression approach
+
+!(() => {
+  console.log('Ooh, fancy.');
+})();
+
+// private variables
+
+(function secretSauce () {
+  let ingredients = [
+    '1/4 cup mayonnaise',
+    '1 1/2 teaspoons spicy pickle juice',
+    '1 1/2 teaspoons ketchup',
+    '1 teaspoon yellow mustard',
+    '1/4 teaspoon smoked paprika',
+    '1/4 teaspoon garlic powder',
+    '1/4 teaspoon onion powder'
+  ];
+
+  mix();
+
+  function mix () {
+    console.log(ingredients.join(', '));
+  }
+}());
+
+// calculated variables
+
+const data = (async () => await fetch('https://api.secret-sauce.club/'))();
+
+// iffy-wiffy with a paramy-wammy
+
+((call, repeat) => {
+  for (let i = 0; i < repeat; i++) call();
+})();
